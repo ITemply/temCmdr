482420f98c877a72a5016942912135727040b0c7
ITemply <120138779+ITemply@users.noreply.github.com>
2024-06-22 11:34:48 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -267,7 +267,7 @@ addCommand('kill', function(args)
 
 	repeat
 	    damageEvent:FireServer(unpack(damageArgs))
-	    client.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0, 10, 0)
+	    client.Character.HumanoidRootPart.CFrame = player.Character.HumanoidRootPart.CFrame * CFrame.new(0, 10, -5)
 	    task.wait()
 	until player.Character.Humanoid.Health <= 0 or client.Character.Humanoid.Health <= 0 
 
